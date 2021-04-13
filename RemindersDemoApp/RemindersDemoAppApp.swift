9d2c5df1766114f739d2e1ccf52ece9a1e4f5b98
Hellen GH <hellensoloviy@gmail.com>
2021-04-13 12:15:14 +0300

    Initial Commit

---
+++ RemindersDemoApp/RemindersDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersDemoAppApp.swift
+//  RemindersDemoApp
+//
+//  Created by Olena Solovii on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RemindersDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
